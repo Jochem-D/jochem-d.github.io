777c07540ddaa4fc3b553d5d7b85972b0ff190bc
Jochem <dijk0146@hz.nl>
2021-09-15 10:46:38 +0200

    fix navbar + ordering

--- javascript/profile.js
+++ javascript/profile.js
@@ -4,3 +4,11 @@ function toggleNav() {
     sidenav.style.width = sidenav.style.width === "250px" ? '0' : '250px';
     main.style.marginLeft = main.style.marginLeft === "250px" ? '0' :  '250px';
 }
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
