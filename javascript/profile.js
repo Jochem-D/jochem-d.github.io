aa41235ccb519b9b6201067d8f44ed719686b426
Jochem <dijk0146@hz.nl>
2021-10-10 11:45:14 +0200

    photo change

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,8 +1,8 @@
 function toggleNav() {
     var sidenav = document.getElementById("mySidebar"),
-    main = document.getElementById("main");
+        main = document.getElementById("main");
     sidenav.style.width = sidenav.style.width === "250px" ? '0' : '250px';
-    main.style.marginLeft = main.style.marginLeft === "250px" ? '0' :  '250px';
+    main.style.marginLeft = main.style.marginLeft === "250px" ? '0' : '250px';
 }
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -10,5 +10,5 @@ function openNav() {
 }
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("main").style.marginLeft = "0";
 }
