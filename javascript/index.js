ad44c5f2260db82f05b4e1639e2f2995b18b8452
Jochem-D <dijk0146@hz.nl>
2025-09-12 14:49:32 +0200

    rollback

--- javascript/index.js
+++ javascript/index.js
@@ -84,11 +84,3 @@ function openWinBox(url, title, width = 800, height = 550) {
 }
 
 
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".close-btn").forEach(btn => {
-    btn.addEventListener("click", e => {
-      e.stopPropagation(); // so click doesn’t open anything
-      btn.closest(".icon").style.display = "none";
-    });
-  });
-});
